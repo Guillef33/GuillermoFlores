c2d2a1e2f8bd111e7a5c5acac626a7ac7a10e5fd
Guillef33 <guillef33@gmail.com>
2021-02-21 21:52:44 -0300

    Ajuste en contacto.php
    Formulario en index.html
    Formulario en contacto.html

--- contacto.php
+++ contacto.php
@@ -1,30 +1,46 @@
 <?php
 
-if (!empty($_POST)) {
+if ($_POST['butonCTA']) {
 
-$firstName = $_POST ["form-firstName"]; 
-$lastName = $_POST ["form-lastName"];
-$company = $_POST ["form-company"];
-$city = $_POST ["form-city"];
-$email = $_POST ["form-email"];
-$mensaje = $_POST ["form-mensaje"];
+        if (!empty($_POST)) {
 
-echo "<br>$firstName";
-echo "<br>$lastName";
-echo "<br>$company";
-echo "<br>$city";
-echo "<br>$email";
-echo "<br>$mensaje";
+        $suscribe = $_POST ["form-suscribe"]; 
 
-if ($firstName == "" || $lastName == "" ||  $company == "" || $city == "" || $email == "" || $mensaje == "") {
-    echo "Error. Completar todos los campos";
-    } else {
-    if(strlen($mensaje) < 10) {
-        echo "Por favor, ingrese una consulta mas amplia";
+        echo "<br>$suscribe";
+
+        if ($suscribe == "" ) {
+            echo "Error. Completar todos los campos";
+            } 
+        }
+} 
+ if ($_POST['contactoBtn']) {
+
+    if (!empty($_POST)) {
+
+        $firstName = $_POST ["form-firstName"]; 
+        $lastName = $_POST ["form-lastName"];
+        $company = $_POST ["form-company"];
+        $city = $_POST ["form-city"];
+        $email = $_POST ["form-email"];
+        $mensaje = $_POST ["form-mensaje"];
+
+        echo "<br>$firstName";
+        echo "<br>$lastName";
+        echo "<br>$company";
+        echo "<br>$city";
+        echo "<br>$email";
+        echo "<br>$mensaje";
+
+        if ($firstName == "" || $lastName == "" ||  $company == "" || $city == "" || $email == "" || $mensaje == "") {
+            echo "Error. Completar todos los campos";
+            } else {
+            if(strlen($mensaje) < 10) {
+                echo "Por favor, ingrese una consulta mas amplia";
+                }
+            }
         }
     }
 
-}
 
 
 ?>
